73df20f19179b97fc1df46b6d82aee9570efa2a8
Jean Sung <jsung@g.hmc.edu>
2013-06-06 13:23:43 -0700

    added units test files, tests are commented out until we figure out how to do them properly

---
+++ trunk/ActionFractions/ActionFractionsTests/ActionFractionsTests.m
@@ -0,0 +1,32 @@
+//
+//  ActionFractionsTests.m
+//  ActionFractionsTests
+//
+//  Created by Jean Sung on 6/6/13.
+//
+//
+
+#import "ActionFractionsTests.h"
+
+@implementation ActionFractionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ActionFractionsTests");
+}
+
+@end
